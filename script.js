2f6f114aa291981b34a40f9116fab6d4bde9e0ef
Chiel Habils <chiel.habils@student.ehb.be>
2020-05-17 18:18:00 +0200

    basic card creation

--- script.js
+++ script.js
@@ -9,13 +9,51 @@ fetch('entries.json')
         )
     .then(
         //() => console.log(jsondata)
-        data => makeCategories(data.items)
+        data => makeCards(data.items)
         )
 
-function makeCategories(data){
-    for (voorstelling in data){
-        
+function makeCards(data){
+    for (let voorstelling of data){
+        // if(data[voorstelling].category == "volwassenen"){
+        //     console.log("volwassenen")
+        // }else if(data[voorstelling].category == "familie"){
+        //     console.log("familie")
+        // }
+        let voorstellingCard = document.createElement("div");
+        let cardImgWrapper = document.createElement("div");
+        let cardContent = document.createElement("div");
+        let cardImgOverlay = document.createElement("div");
+        let cardGenre = document.createElement("div");
+        let cardExcerpt = document.createElement("div");
+        let cardTitle = document.createElement("H3");
+        let cardImg = document.createElement("img");
+
+
+
+        voorstellingCard.className = "card";
+        cardImgWrapper.className = "cardImgWrapper";
+        cardContent.className = "cardContent";
+        cardImg.className = "cardImg";
+        cardImgOverlay.className = "cardImgOverlay";
+        cardGenre.className = "cardGenre";
+        cardExcerpt.className = "cardExcerpt";
+
+        cardTitle.innerHTML = voorstelling.name;
+        cardImg.src = voorstelling.thumbnail.url;
+        cardGenre.innerHTML = voorstelling['genre-v2'];
+        cardExcerpt.innerHTML = voorstelling.excerpt;
+
+        cardImgOverlay.appendChild(cardGenre);
+        cardContent.appendChild(cardTitle);
+        cardImgWrapper.appendChild(cardImg);
+        cardImgWrapper.appendChild(cardImgOverlay);
+        cardContent.appendChild(cardExcerpt);
+
+        voorstellingCard.appendChild(cardImgWrapper);
+        voorstellingCard.appendChild(cardContent);
+
+
+		document.getElementById("results").appendChild(voorstellingCard);
     }
-    console.log(data)
 
 }
