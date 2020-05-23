48627d94037ff91a3298cedc91fd444fb3f0309b
Chiel Habils <chiel.habils@student.ehb.be>
2020-05-23 22:22:10 +0200

    Searchbar implemented + comments

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# DevIVFilter
-Eindwerk Dev IV Multec EHB
+# Functional analysis of existing app
+    De bestaande website toont opnames van voorstellingen. Deze worden uit een database gehaald en opgebouwd in de vorm van cards. 
+
+    Er zijn twee mogelijkheden aanwezig om te filteren door deze cards. 
+    De eerste die we zien is een searchbar. Wanneer we hierin typen komen kleine versies van de cards tevoorschijn. De searchbar geeft cards met titels weer gebaseerd op de input. 
+    De tweede mogelijkheid is een reeks van filterknoppen. Deze zijn onderverdeeld in doelgroepen (Volwassenen, familie) en genres (Theater, dans, ...). Wanneer we 1 knop aanklikken worden alleen de cards die daarmee overeenkomen getoond. We kunnen ook meerdere knoppen tegelijk aanklikken. Daarbij worden alle cards die minstens met 1 knop overeenkomen getoond. 
+
+# Planning of own app
+    Eerst ga ik alle data ophalen van de json file. Deze data wordt
+    dan gebruikt om met een for-loop alle cards aan te maken. De cards die we zien bij de searchbar ga ik niet meteen tonen, die bij de filter knoppen wel.
+
+    Dan wil ik alle filter knoppen aanmaken op basis van de json data. Zo ben ik zeker dat als er nieuwe categorieen of genres zijn, deze ook te zien zijn op de website.
+
+    Daarna ga ik een event listener maken die checkt wanneer een filter knop aangeklikt wordt. Wanneer dat gebeurt, haal ik alle bestaande cards op, itereer ik daardoor en check of ze voldoen aan de voorwaarden van de filter knoppen. Als dit zo is worden ze getoond, anders niet. 
+
+    Wanneer dit allemaal werkt, ga ik de searchbar implementeren. Daar voeg ik een event listener aan toe die checkt of er in getypt wordt. Wanneer dat zo is, toon ik bij elke getypte letter alle cards wiens titels overeenkomen met de query in de searchbar input. 
+    
+ 
--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let jsondata;
 let doelgroepen = [];
 let genres = [];
 
+//Fetching data
 fetch('entries.json')
     .then(
         res => res.json()
@@ -14,8 +15,13 @@ fetch('entries.json')
         data => makeCards(data.items)
         )
 
+//Making all the cards
 function makeCards(data){
+
+    //loop to make every object in json file into a card
     for (let voorstelling of data){
+
+        //creating Category buttons
         if(!doelgroepen.includes(voorstelling.category)){
             doelgroepen.push(voorstelling.category)
             let doelgroepBtn = document.createElement("button");
@@ -24,6 +30,7 @@ function makeCards(data){
             document.getElementById("doelgroepbtns").appendChild(doelgroepBtn);
         }
 
+        //creating Genre Buttons
         if(!genres.includes(voorstelling['genre-v2'])){
             genres.push(voorstelling['genre-v2'])
             let genreBtn = document.createElement("button");
@@ -32,6 +39,7 @@ function makeCards(data){
             document.getElementById("genrebtns").appendChild(genreBtn);
         }
 
+        //creating all elements for a standard card
         let voorstellingCard = document.createElement("div");
         let cardCategory = document.createElement("div");
         let cardImgWrapper = document.createElement("div");
@@ -43,6 +51,17 @@ function makeCards(data){
         let cardImg = document.createElement("img");
         let cardRecorded = document.createElement("div");
 
+        //creating all elements for a searchbar card
+        let horizontalCard = document.createElement("div");
+        let horizontalWrapper = document.createElement("div");
+        let horizontalTitle = document.createElement("H3");
+        let horizontalContent = document.createElement("div");
+        let horizontalExcerpt = document.createElement("div");
+        let horizontalImg = document.createElement("img");
+        let horizontalRecorded = document.createElement("div");
+        let horizontalImgOverlay = document.createElement("div");
+
+        //adding classes to all the elements of the standard card
         voorstellingCard.className = "card";
         cardImgWrapper.className = "cardImgWrapper";
         cardContent.className = "cardContent";
@@ -51,8 +70,18 @@ function makeCards(data){
         cardGenre.className = "cardGenre";
         cardExcerpt.className = "cardExcerpt";
         cardRecorded.className = "cardExcerpt";
-        cardCategory.className = "cardCategory"
+        cardCategory.className = "cardCategory";
 
+        //adding classes to all the elements of the searchbar card
+        horizontalCard.className = "horizontalCard";
+        horizontalWrapper.className = "horizontalWrapper";
+        horizontalContent.className = "horizontalContent";
+        horizontalImg.className = "horizontalImg";
+        horizontalExcerpt.className = "horizontalExcerpt";
+        horizontalRecorded.className = "horizontalRecorded";
+        horizontalImgOverlay.className = "horizontalImgOverlay";
+
+        //adding data from the json object to the standard card elements
         cardTitle.innerHTML = voorstelling.name;
         cardCategory.innerHTML = voorstelling.category;
         cardImg.src = voorstelling.thumbnail.url;
@@ -60,6 +89,26 @@ function makeCards(data){
         cardExcerpt.innerHTML = voorstelling.excerpt;
         cardRecorded.innerHTML = voorstelling['recorded-at'];
 
+        //adding data from the json object to the searchbar card elements
+        horizontalTitle.innerHTML = voorstelling.name;
+        horizontalImg.src = voorstelling.thumbnail.url;
+        horizontalExcerpt.innerHTML = voorstelling.excerpt;
+        horizontalRecorded.innerHTML = voorstelling['recorded-at'];
+
+        //Appending all the elements to the searchbar card to build it
+        horizontalContent.appendChild(horizontalTitle);
+        horizontalContent.appendChild(horizontalExcerpt);
+        if(voorstelling['recorded-at'] != undefined){
+            horizontalContent.appendChild(horizontalRecorded);
+        }
+        horizontalWrapper.appendChild(horizontalImg);
+        horizontalWrapper.appendChild(horizontalImgOverlay);
+        horizontalCard.appendChild(horizontalWrapper);
+        horizontalCard.appendChild(horizontalContent);
+        horizontalCard.style.display = 'none';
+
+
+        //Appending all the elements to the standard card to build it
         cardImgOverlay.appendChild(cardGenre);
         cardContent.appendChild(cardTitle);
         cardImgWrapper.appendChild(cardImg);
@@ -69,16 +118,19 @@ function makeCards(data){
         if(voorstelling['recorded-at'] != undefined){
             cardContent.appendChild(cardRecorded);
         }
-    
         voorstellingCard.appendChild(cardImgWrapper);
         voorstellingCard.appendChild(cardContent);
 
-		document.getElementById("results").appendChild(voorstellingCard);
+        //adding the two cards to the webpage
+        document.getElementById("results").appendChild(voorstellingCard);
+        document.getElementById("searchresults").appendChild(horizontalCard);
     }
 
+    //Event listener to check if filter buttons are clicked
     document.addEventListener("click", function(e){
         let element = e.target
 
+        //on and off switching of buttons and changing active state
         if (element.classList.contains('categoryBtn')){
          if(element.classList.contains('active')){
             element.classList.remove("active");
@@ -94,6 +146,7 @@ function makeCards(data){
          }
         }
 
+        //checking which buttons have been activated
          var activeFilters = document.getElementsByClassName("active");
          var activeCategories = [];
          var activeGenres = [];
@@ -107,18 +160,22 @@ function makeCards(data){
              
          }
 
+         //getting all standard cards to check their data
          var allCards = document.getElementsByClassName("card");
          for(let thisCard of allCards){
 
+            //getting genre from card to compare later
              var imgWrapper = thisCard.childNodes[0];
              var imgOverlay = imgWrapper.childNodes[1];
              var genre = imgOverlay.childNodes[0];
              var genreText = genre.innerHTML;
 
+             //getting category from card to compare later
              var cardContent = thisCard.childNodes[1];
              var category = cardContent.childNodes[2];
              var categoryText = category.innerHTML;
              
+             //Comparing genre/category of card to activated buttons and showing the ones that match
              if(activeCategories.length > 0 && activeGenres.length == 0 ){
                 if(activeCategories.includes(categoryText)){
                     thisCard.style.display = 'block';
@@ -143,12 +200,37 @@ function makeCards(data){
             }
          })
 
-    const input = document.querySelector('input');
-    input.addEventListener('input', updateSearch);
+    //getting the searchbar and adding event listener on typing
+    const input = document.querySelector('input');  
+    input.addEventListener('input', updateValue);
+    
+    //function to show cards when typing in searchbar
+    function updateValue() {
 
-    function updateSearch(e) {
+        //getting the input from searchbar
+        let currentInput = document.getElementById('searchInput').value;
 
+        //getting all searchbar cards
+        var allHorizontalCards = document.getElementsByClassName("horizontalCard");
+
+        //loop to compare all searchbar cards to input value
+        for(let thisCard of allHorizontalCards){
+
+            //getting title from card
+            var cardContent = thisCard.childNodes[1];
+            var title = cardContent.childNodes[0];
+            var titleText = title.innerHTML;
+
+            //lowercasing both title and input to make searchbar case insensitive (like the real website)
+            var titleLowercase = titleText.toLowerCase();
+            var currentInputLowercase = currentInput.toLowerCase();
+
+            //comparing card title to input and showing the ones that match
+            if(titleLowercase.includes(currentInputLowercase) && currentInput.length > 0){
+               thisCard.style.display = 'flex';
+            }else{
+                thisCard.style.display = 'none';
+            }
+           }
     }
-
-
 }
